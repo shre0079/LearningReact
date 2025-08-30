8e4d2d4df58c9ddba96290bdb20670dd0803d0f7
Shreyash <shreyashshree0079@gmail.com>
2025-08-31 01:41:54 +0530

    conditional statements

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -18,6 +18,7 @@ function ListGroup() {
   return (
     <>
       <h1>List</h1>
+        {items.length===0 ? <p>No item found</p> : null}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
