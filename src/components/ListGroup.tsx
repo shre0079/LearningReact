e42cc0e9c03a02aa37465822fd730f854b65602b
Shreyash <shreyashshree0079@gmail.com>
2025-08-31 01:11:13 +0530

    conditional rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -7,6 +7,14 @@ function ListGroup() {
     "California",
   ];
 
+  if (items.length === 0)
+    return (
+      <>
+        {" "}
+        <h1>List</h1> <p>No items found</p>
+      </>
+    );
+
   return (
     <>
       <h1>List</h1>
