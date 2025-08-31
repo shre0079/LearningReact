67be27c02e11d5405a3cf6788e813d707c05b49b
Shreyash <shreyashshree0079@gmail.com>
2025-08-31 13:55:34 +0530

    event handling

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -18,7 +18,7 @@ function ListGroup() {
         {items.length===0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item) => (
-          <li key={item}>{item}</li>
+          <li className="list-group-item" onClick={()=>console.log("Clicked")} key={item}>{item}</li>
         ))}
       </ul>
     </>
